680c07ff1090fc19263bdb21d938d6794e8039a5
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-29 21:12:44 -0600

    Change to standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { NewTodoComponent } from './new-todo/new-todo.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [HeaderComponent, NewTodoComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';

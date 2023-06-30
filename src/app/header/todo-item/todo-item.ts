7862234358489d03ebef093c8ea4375d25013227
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-06-29 21:13:39 -0600

    Create todo-item.ts

---
+++ src/app/header/todo-item/todo-item.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+})
+export class TodoItemComponent {}

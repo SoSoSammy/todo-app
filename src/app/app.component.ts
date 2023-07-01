8a6b1c88556d3103f5564e574cb5e186ae59d957
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-01 11:35:32 -0600

    Import TodoListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
 import { NewTodoComponent } from './new-todo/new-todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HeaderComponent, NewTodoComponent],
+  imports: [HeaderComponent, NewTodoComponent, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })

e1f2f61f84cc9d7e7efbe79ef4c25f74c13fe4f0
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-01 20:15:17 -0600

    Create todo-filter.component.ts

---
+++ src/app/todo-filter/todo-filter.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-todo-filter',
+  templateUrl: './todo-filter.component.html',
+})
+export class TodoFilterComponent {}

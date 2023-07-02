e3ceee319cc594e71b6c4cad94a3624f0c13d9e3
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-01 20:15:33 -0600

    Import TodoFilterComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { NewTodoComponent } from './new-todo/new-todo.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFilterComponent } from './todo-filter/todo-filter.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HeaderComponent, NewTodoComponent, TodoListComponent],
+  imports: [
+    HeaderComponent,
+    NewTodoComponent,
+    TodoListComponent,
+    TodoFilterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })

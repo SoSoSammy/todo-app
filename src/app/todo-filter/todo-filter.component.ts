fb9a9366ff8de6897294fb1a9c656d74c0e2e039
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-07-03 18:16:32 -0600

    Filter todos on click

--- src/app/todo-filter/todo-filter.component.ts
+++ src/app/todo-filter/todo-filter.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoService } from '../todo.service';
 
 @Component({
   standalone: true,
   selector: 'app-todo-filter',
+  imports: [CommonModule],
   templateUrl: './todo-filter.component.html',
 })
-export class TodoFilterComponent {}
+export class TodoFilterComponent {
+  selectedFilter = 'all';
+
+  constructor(private todoService: TodoService) {}
+
+  filterAll() {
+    this.todoService.filterTodos('all');
+    this.selectedFilter = 'all';
+  }
+
+  filterActive() {
+    this.todoService.filterTodos('active');
+    this.selectedFilter = 'active';
+  }
+
+  filterCompleted() {
+    this.todoService.filterTodos('completed');
+    this.selectedFilter = 'completed';
+  }
+}

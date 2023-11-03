77d9a8a8fbf4c3722f1a2507d3b397b14747adec
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-11-03 08:13:54 -0600

    Add comments

--- src/app/todo-list/todo-details/todo-details.component.ts
+++ src/app/todo-list/todo-details/todo-details.component.ts
@@ -11,9 +11,15 @@ import { TodoService } from 'src/app/todo.service';
   templateUrl: './todo-details.component.html',
 })
 export class TodoDetailsComponent {
-  remainingTodos = this.todoService.getTodosLeft();
-  subscription: Subscription;
+  remainingTodos = this.todoService.getTodosLeft(); // Number of todos not completed
+  subscription: Subscription; // Subscription to todosChanged
 
+  /**
+   * Build the todo details component with its necessary dependencies, subscribe to todosChanged and update the
+   * remaining todos whenever todos change.
+   *
+   * @param todoService - the todo service
+   */
   constructor(private todoService: TodoService) {
     // Update remaining todos when todos change
     this.todoService.todosChanged.subscribe(() => {
@@ -21,6 +27,9 @@ export class TodoDetailsComponent {
     });
   }
 
+  /**
+   * Clear all completed todos.
+   */
   clearCompleted() {
     this.todoService.clearCompleted();
   }

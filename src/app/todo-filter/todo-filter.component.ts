157af321827b0e9953f0c1fcd9b99ca60283c8a9
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-11-13 07:08:12 -0700

    Revert "Get selected filter from todo service"
    This reverts commit 018482a75540e9f46f0b63733bb3ce8834792662.

--- src/app/todo-filter/todo-filter.component.ts
+++ src/app/todo-filter/todo-filter.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
 
 import { TodoService } from '../todo.service';
 
@@ -11,42 +10,22 @@ import { TodoService } from '../todo.service';
   templateUrl: './todo-filter.component.html',
 })
 export class TodoFilterComponent {
-  selectedFilter: string; // The selected filter for the todo list
-  subscription: Subscription; // Subscription to filterChanged
+  selectedFilter = 'all';
 
-  /**
-   * Build the todo filter component with its necessary dependencies, subscribe to filterChanged and update the
-   * selected filter whenever the filter changes, and get the initial filter.
-   *
-   * @param todoService - the todo service
-   */
-  constructor(private todoService: TodoService) {
-    this.subscription = this.todoService.filterChanged.subscribe(
-      (filter: string) => {
-        this.selectedFilter = filter;
-      }
-    );
-    this.selectedFilter = this.todoService.getSelectedFilter();
-  }
+  constructor(private todoService: TodoService) {}
 
-  /**
-   * Filter the todo list by all todos.
-   */
   filterAll() {
     this.todoService.filterTodos('all');
+    this.selectedFilter = 'all';
   }
 
-  /**
-   * Filter the todo list by active todos.
-   */
   filterActive() {
     this.todoService.filterTodos('active');
+    this.selectedFilter = 'active';
   }
 
-  /**
-   * Filter the todo list by completed todos.
-   */
   filterCompleted() {
     this.todoService.filterTodos('completed');
+    this.selectedFilter = 'completed';
   }
 }

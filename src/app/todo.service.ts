c9eb95211196d423f8f77fc8b8fcafe6412f50ed
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2023-11-13 07:08:27 -0700

    Revert "Add selected filter variable and subject"
    This reverts commit 55915e3047e84d6734308de62fd7c40dd090577b.

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,8 +1,7 @@
 import { Subject } from 'rxjs';
 
 export class TodoService {
-  todosChanged = new Subject<Object[]>(); // A subject to notify subscribers when todos change
-  filterChanged = new Subject<string>(); // A subject to notify subscribers when the selected filter changes
+  todosChanged = new Subject<Object[]>();
 
   private todos = [
     { title: 'Complete online JavaScript course', completed: true },
@@ -12,107 +11,55 @@ export class TodoService {
     { title: 'Pick up groceries', completed: false },
     { title: 'Complete Todo App on Frontend Mentor', completed: false },
   ];
-  private selectedFilter = 'all'; // The selected filter for the todo list
 
-  /**
-   * Gets the todos.
-   *
-   * @returns a copy of the todos array
-   */
   getTodos() {
-    return this.todos.slice();
+    return this.todos.slice(); // return a copy of the array
   }
 
-  /**
-   * Gets the number of todos that are not completed.
-   *
-   * @returns the number of todos that are not completed
-   */
+  // Get the number of todos that are not completed
   getTodosLeft() {
     return this.todos.reduce((count, todo) => {
       return count + (todo.completed ? 0 : 1);
     }, 0);
   }
 
-  /**
-   * Adds a todo.
-   *
-   * @param todo - the todo to add
-   */
   addTodo(todo: string) {
     this.todos.push({ title: todo, completed: false });
-    // Filter the todos with the current filter and notify subscribers that the todos
-    // have changed
-    this.filterTodos(this.selectedFilter);
+    // Notify subscribers that the todos have changed
+    this.todosChanged.next(this.todos.slice());
   }
 
-  /**
-   * Completes a todo.
-   *
-   * @param index - the index of the todo to complete
-   */
   completeTodo(index: number) {
     this.todos[index].completed = !this.todos[index].completed;
-    // Filter the todos with the current filter and notify subscribers that the todos
-    // have changed
-    this.filterTodos(this.selectedFilter);
+    // Notify subscribers that the todos have changed
+    this.todosChanged.next(this.todos.slice());
   }
 
-  /**
-   * Deletes a todo.
-   *
-   * @param index - the index of the todo to delete
-   */
   deleteTodo(index: number) {
     this.todos.splice(index, 1);
-    // Filter the todos with the current filter and notify subscribers that the todos
-    // have changed
-    this.filterTodos(this.selectedFilter);
+    // Notify subscribers that the todos have changed
+    this.todosChanged.next(this.todos.slice());
   }
 
-  /**
-   * Clears all completed todos.
-   */
   clearCompleted() {
     this.todos = this.todos.filter((todo) => !todo.completed);
-    // Filter the todos with the current filter and notify subscribers that the todos
-    // have changed
-    this.filterTodos(this.selectedFilter);
+    // Notify subscribers that the todos have changed
+    this.todosChanged.next(this.todos.slice());
   }
 
-  /**
-   * Gets the selected filter.
-   *
-   * @returns the selected filter
-   */
-  getSelectedFilter() {
-    return this.selectedFilter;
-  }
-
-  /**
-   * Filters the todos and changes the selected filter.
-   *
-   * @param filter - the filter to apply to the todos
-   */
   filterTodos(filter: string) {
     switch (filter) {
       case 'all':
-        this.selectedFilter = 'all';
-        this.filterChanged.next(this.selectedFilter);
-        this.todosChanged.next(this.todos.slice());
-        break;
+        return this.todosChanged.next(this.todos.slice());
       case 'active':
-        this.selectedFilter = 'active';
-        this.filterChanged.next(this.selectedFilter);
-        this.todosChanged.next(this.todos.filter((todo) => !todo['completed']));
-        break;
+        return this.todosChanged.next(
+          this.todos.filter((todo) => !todo['completed'])
+        );
       case 'completed':
-        this.selectedFilter = 'completed';
-        this.filterChanged.next(this.selectedFilter);
-        this.todosChanged.next(this.todos.filter((todo) => todo['completed']));
-        break;
+        return this.todosChanged.next(
+          this.todos.filter((todo) => todo['completed'])
+        );
       default:
-        this.filterChanged.next(this.selectedFilter);
         this.todosChanged.next(this.todos.slice());
     }
   }
